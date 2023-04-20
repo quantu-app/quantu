13d911f8b847a4b0af1cad2a4253108586b705c1
John Faucett <jsfaucett87@icloud.com>
2023-04-20 17:09:19 +0200

    Add tailwindcss to storybook

---
+++ src/lib/components/theme/Button.stories.ts
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import Button from './Button.svelte';
+
+const meta = {
+  title: 'Theme/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+  },
+} satisfies Meta<Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
+export const Primary: Story = {
+  args: {
+    label: 'Save',
+  },
+};
---
+++ src/lib/components/theme/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  
+  export let label: string;
+
+</script>
+
+<button
+  type="button"
+  class="px-4 py-2 rounded-lg bg-slate-800 text-white"
+  on:click
+  >{label}</button>
