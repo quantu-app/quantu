8d9c5b65245f2999910af607ca285b48b314ddcb
John Faucett <jsfaucett87@icloud.com>
2023-05-07 14:54:08 +0200

    login/logout wip

--- src/lib/components/app/Navbar.svelte
+++ src/lib/components/app/Navbar.svelte
@@ -26,25 +26,26 @@
         </div>
 
         <!-- USER MENU -->
-        <div class="navbar-end">
-          <div class="avatar avatar-ring avatar-md">
-            <div class="dropdown-container">
-              <div class="dropdown">
-                <label class="btn btn-ghost flex cursor-pointer px-0" tabindex="0">
-                  <img src="https://i.pravatar.cc/150?u=a042581f4e29026024d" alt="avatar" />
-                </label>
-                <div class="dropdown-menu dropdown-menu-bottom-left">
-                  <a class="dropdown-item text-sm">Profile</a>
-                  <a tabindex="-1" class="dropdown-item text-sm">Account settings</a>
-                  {#if user && user.creator}
-                  <a tabindex="-1" class="dropdown-item text-sm">Studio</a>
-                  {/if}
-                  <a tabindex="-1" class="dropdown-item text-sm">Logout</a>
+        {#if user}
+          <div class="navbar-end">
+            <div class="avatar avatar-ring avatar-md">
+              <div class="dropdown-container">
+                <div class="dropdown">
+                  <label class="btn btn-ghost flex cursor-pointer px-0" tabindex="0">
+                    <img src="https://i.pravatar.cc/150?u=a042581f4e29026024d" alt="avatar" />
+                  </label>
+                  <div class="dropdown-menu dropdown-menu-bottom-left">
+                    <a href={`${base}/account/settings`} tabindex="-1" class="dropdown-item text-sm">Account settings</a>
+                    {#if user.creator}
+                      <a href={`${base}/studio`} tabindex="-1" class="dropdown-item text-sm">Studio</a>
+                    {/if}
+                    <a href={`${base}/logout`} tabindex="-1" class="dropdown-item text-sm">Logout</a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        {/if}
       </div>
   </nav>
   </header>
---
+++ src/routes/(authed)/logout/+server.ts
@@ -0,0 +1,14 @@
+import { base } from "$app/paths";
+import { redirect } from "@sveltejs/kit";
+import type { RequestHandler } from "./$types";
+
+export const GET: RequestHandler = async ({cookies}) => {
+
+  const token = cookies.get("token")
+  
+  if (token) {
+    cookies.delete("token");
+  }
+
+  throw redirect(303, `${base}/`);
+}
